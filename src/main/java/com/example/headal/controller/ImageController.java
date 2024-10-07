6d30eea5c76f337e712c2b39075cdf81be3d12c9
Han <jumdo12@gmail.com>
2024-10-07 19:05:19 +0900

    Haedal

---
+++ src/main/java/com/example/headal/controller/ImageController.java
@@ -0,0 +1,31 @@
+package com.example.headal.controller;
+
+import com.example.headal.domain.User;
+import com.example.headal.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public class ImageController {
+    private final AuthService authService;
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(AuthService authService, ImageService imageService) {
+        this.authService = authService;
+        this.imageService = imageService;
+    }
+
+    @PutMapping("/users/image")
+    public ResponseEntity<String> updateUserImage(@RequestParam("image") MultipartFile image, HttpServletRequest request) throws IOException {
+        User currentUser = authService.getCurrentUser(request);
+
+        String savedImageName = imageService.updateUserImage(currentUser, image);
+        return ResponseEntity.ok(savedImageName);
+    }
+}
